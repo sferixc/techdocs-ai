c17418ef6fc2021b06570bb4995cb48b822b28e2
Eric Iencsi <eric.gabriel705@gmail.com>
2026-08-28 16:21:30 +0200

    Initialize Spring Boot and React project

---
+++ src/test/java/com/eric/techdocsai/TechdocsAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eric.techdocsai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechdocsAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
